b60a9f8f46da818f67607c3e4e375ef0ebed1887
Luka Kresoja <57074484+LukeOnuke@users.noreply.github.com>
2023-10-29 01:23:12 +0200

    Add damage clause for projectiles

--- src/main/java/com/lukeonuke/pvptoggle/event/OnDamageListener.java
+++ src/main/java/com/lukeonuke/pvptoggle/event/OnDamageListener.java
@@ -1,7 +1,10 @@
 package com.lukeonuke.pvptoggle.event;
 
 import com.lukeonuke.pvptoggle.service.PvpService;
+import org.bukkit.entity.EnderCrystal;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
@@ -11,10 +14,18 @@ public class OnDamageListener implements Listener {
     //fired when an entity is hit
     public void onHit(EntityDamageByEntityEvent event) {
         if(!(event.getEntity() instanceof Player)) return;
-        if(!(event.getDamager() instanceof Player)) return;
+        Player player = (Player)event.getDamager();
+        if(event.getDamager() instanceof Player) {
+            if(!PvpService.isPvpEnabled(player) || !PvpService.isPvpEnabled((Player)event.getEntity())){
+                event.setCancelled(true);
+            }
+        }
 
-        if(!PvpService.isPvpEnabled((Player)event.getDamager()) || !PvpService.isPvpEnabled((Player)event.getEntity())){
-            event.setCancelled(true);
+        if(event.getDamager() instanceof Projectile){
+            if(event.getDamager() instanceof Player projectileOwner) {
+                if(!PvpService.isPvpEnabled(player)) event.setCancelled(true);
+                if(!PvpService.isPvpEnabled(projectileOwner)) event.setCancelled(true);
+            }
         }
     }
 }
